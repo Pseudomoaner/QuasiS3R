48771edeaaeb99cfc9b4935fdfbb417ea4066809
Oliver Meacock <olijme2@hotmail.com>
2022-05-12 17:59:56 +0200

    post-simulation rendering updates

--- Plotting/doBatchColourReconstructs.m
+++ Plotting/doBatchColourReconstructs.m
@@ -1,30 +1,18 @@
 clear all
 close all
 
-Root = 'D:\SegregationPaper\MixtureSims\PackFrac_0_5';
-genericBranch = 'cellForces_%s_%s_aspectRatios_%s_%s_Repeat_1';
+Root = '/home/omeacock/Documents/SPRruns/Fig1Runs/';
+inBranches = {'Patchy','Nonpatchy','FrozenPatchy','FrozenNonpatchy'};
 inTwig = 'SimulationResults.mat';
-outTwig = 'Reconstructs';
+outBranch = 'ColourCells';
+outTwig = 'Frame_%04d.tif';
 
-F1 = 1;
-A1 = 4;
+dispSettings.ImgPath = outTwig;
+dispSettings.colourCells = 'Hits';
 
-Forces = [2];
-AspRats = [5];
+for r = 1:size(inBranches,2)
+    load(fullfile(Root,inBranches{r},inTwig),'endField','trackableData')
+    dispSettings.imagedirectory = [Root,inBranches{r},filesep,outBranch];
 
-Branches = cell(size(Forces,2)*size(AspRats,2),1);
-
-count = 1;
-for f = 1:size(Forces,2)
-    for a = 1:size(AspRats,2)
-        Branches{count} = sprintf(genericBranch,num2str(Forces(f)),num2str(F1),num2str(AspRats(a)),num2str(A1));
-        count = count+1;
-    end
-end
-
-for r = 1:size(Branches,1)
-    load([Root,filesep,Branches{r},filesep,inTwig],'fieldSettings','dispSettings','trackableData')
-    dispSettings.imagedirectory = [Root,filesep,Branches{r},filesep,outTwig];
-
-    paintFieldReconstructionTwoPops(trackableData,dispSettings,fieldSettings)
+    paintFieldReconstruction(trackableData,dispSettings,endField)
 end
--- Plotting/paintFieldReconstruction.m
+++ Plotting/paintFieldReconstruction.m
@@ -1,18 +1,65 @@
-function [] = paintFieldReconstruction(trackableData,dS,fS)
+function [] = paintFieldReconstruction(trackableData,dS,eF)
 
-resolution = 0.2; %Equivalent to pixel size - how many cell widths each pixel should correspond to
+Upsample = 8; %Extent to which the 'design' image should be interpolated to create smoother graphics.
+Downsample = 2; %Extent to which the resulting image should be downsized to make the final image size
+majorBoost = 0.2; %Extra factor by which to make cells longer for rendering purposes
+minorBoost = 0.1; %Extra factor by which to make cells wider for rendering purposes           
 
-for i = 1:length(trackableData.Length)
-    cellImg = zeros(round(fS.fieldHeight/resolution),round(fS.fieldWidth/resolution));
-    xs = trackableData.Centroid{i}(:,1);
-    ys = trackableData.Centroid{i}(:,2);
-    phis = rad2deg(trackableData.Orientation{i});
-    majors = (cos(trackableData.Tilt{i}).*(trackableData.Length{i}-fS.lam) + fS.lam)/2;
-    minors = repmat(fS.lam/2,size(xs,1),1);
-    cellImg = paintEllipse(cellImg,xs,ys,majors,minors,phis,resolution);
+for t = 1:length(trackableData.Length)
+    switch dS.colourCells
+        case 'Orientation'
+            map = colormap('hsv');
+            colSet = zeros(size(trackableData.Length{t},1),3);
+            for k = 1:size(trackableData.Orientation{t},1)
+                bin = ceil((mod(trackableData.Orientation{t}(k)+pi,pi)/pi) * size(map,1));
+                colSet(k,:) = map(bin,:);
+            end
+        case 'Position'
+            colSet = zeros(size(trackableData.Length{t},1),3);
+            for k = 1:size(trackableData.Length{t},1)
+                xFac = trackableData.Centroid{t}(k)/eF.xWidth;
+                yFac = trackableData.Centroid{t}(k)/eF.yHeight;
+                colSet(k,:) = [xFac,1-(xFac+yFac)/2,yFac];
+            end
+        case 'Hits' %Choice for debugging purposes - won't necessarily always work with all parameter settings. Assumes a firing and non-firing population.
+            maxHit = 5;
+            colSet = zeros(size(trackableData.Length{t},1),3);
+            for k = 1:size(trackableData.Length{t},1)
+                currHits = min(trackableData.Hit{t}(k),maxHit);
+                if currHits > 0 %Dead cells are set as magenta shading to gray
+                    colFac = (currHits)/(maxHit+1.5);
+                    colSet(k,:) = 1-[1,0.5+colFac*0.5,0.05+colFac*0.95];%[1-colFac,0.8-colFac*0.8,0.05-colFac*0.05];%-[0.9-0.7*colFac,0.2*colFac,0.5-colFac*0.3];%-[0.9,colFac*0.9,0.4+colFac*0.5];
+                elseif trackableData.FireRate{t}(k) > 0 %Firing cells are set as light blue
+                    colSet(k,:) = 1-([32,128,196]/255);
+                elseif trackableData.FireRate{t}(k) == 0 %Unhit cells are set as yellow
+                    colSet(k,:) = 1-([255, 120, 10]/255);
+                end
+            end
+    end
     
-    Imgpath = sprintf(dS.ImgPath3,i);
+    backImg = ones(round(eF.yHeight*Upsample),round(eF.xWidth*Upsample));
+    xs = trackableData.Centroid{t}(:,1);
+    ys = trackableData.Centroid{t}(:,2);
+    phis = rad2deg(trackableData.Orientation{t});
+    majors = (cos(trackableData.Tilt{t}).*(trackableData.Length{t}-eF.lam) + eF.lam)/2 + majorBoost;
+    minors = (eF.lam*ones(size(colSet,1),1)/2)+minorBoost;
+    
+    %Do separate paintjobs for each of the three colour
+    %channels
+    imgr = paintEllipse(backImg,xs,ys,majors,minors,phis,colSet(:,1),1/Upsample,eF.boundConds,eF.xWidth,eF.yHeight);
+    imgg = paintEllipse(backImg,xs,ys,majors,minors,phis,colSet(:,2),1/Upsample,eF.boundConds,eF.xWidth,eF.yHeight);
+    imgb = paintEllipse(backImg,xs,ys,majors,minors,phis,colSet(:,3),1/Upsample,eF.boundConds,eF.xWidth,eF.yHeight);
+    
+    %Smooth to make nicer looking
+    imgr = imgaussfilt(imgr,Upsample*eF.lam/4);
+    imgg = imgaussfilt(imgg,Upsample*eF.lam/4);
+    imgb = imgaussfilt(imgb,Upsample*eF.lam/4);
+    
+    outImg = cat(3,imgr,imgg,imgb);
+    outImg = imresize(outImg,1/Downsample);
+    
+    Imgpath = sprintf(dS.ImgPath,t);
     ImgpathTemp = [dS.imagedirectory, filesep, Imgpath];
     
-    imwrite(cellImg,ImgpathTemp,'TIFF')
+    imwrite(outImg,ImgpathTemp,'TIFF')
 end
